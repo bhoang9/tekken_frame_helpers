826d5a42925cc16097e888f1912cb015a549f742
Brian H <brianbhoang9@gmail.com>
2018-10-24 22:44:11 -0700

    added function to update movelist_id

--- tekken_frame_TA_sql/tekken_frame_sql_helpers.py
+++ tekken_frame_TA_sql/tekken_frame_sql_helpers.py
@@ -1,10 +1,19 @@
 import sqlite3
 
+def create_connection(db_file):
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+        return None
+
 #create table with movelist
 def create_moveList_table():
 	moveList_sql = """
 	CREATE TABLE if not exists moves (
-	id integer,
+        id integer PRIMARY KEY,
+	movelist_id integer,
 	command text,
 	hit_height text,
 	start_frame text,
@@ -25,7 +34,7 @@ def create_name_table():
 #insert move into moveList table
 def insert_move(cursor, move_array):
 	move_insert_sql = """
-	INSERT INTO moves (id, command, hit_height, start_frame, block_frame, hit_frame, damage)
+	INSERT INTO moves (movelist_id, command, hit_height, start_frame, block_frame, hit_frame, damage)
 	VALUES (?,?,?,?,?,?,?)"""
 
 	cursor.execute(move_insert_sql, (move_array[6],move_array[0],move_array[1],move_array[2],
@@ -37,3 +46,12 @@ def insert_name(cursor, name):
 	INSERT INTO name (name) VALUES (?) """
 
 	cursor.execute(name_insert_sql, (name,))
+
+#update move id
+def update_move_id(cursor, old_id, incr_amt):
+        new_id = old_id + incr_amt
+        print(new_id)
+        update_move_id_sql = """
+        UPDATE moves SET movelist_id = ? WHERE id = ? """
+
+        cursor.execute(update_move_id_sql, (new_id, old_id))
